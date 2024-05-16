3324733725d0346dd8219043a19ef0a78b155cc0
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-05-16 15:05:16 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/health_scan_pro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.health_scan_pro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,36 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesArrowCircle
+  /// assets/images/arrow-circle.svg
+  static const String imagesArrowCircle = "assets/images/arrow-circle.svg";
+
+  /// Assets for imagesIconHeart
+  /// assets/images/iconHeart.svg
+  static const String imagesIconHeart = "assets/images/iconHeart.svg";
+
+  /// Assets for imagesIconKidney
+  /// assets/images/iconKidney.svg
+  static const String imagesIconKidney = "assets/images/iconKidney.svg";
+
+  /// Assets for imagesIconLung
+  /// assets/images/iconLung.svg
+  static const String imagesIconLung = "assets/images/iconLung.svg";
+
+  /// Assets for imagesImagesResult
+  /// assets/images/images_result.svg
+  static const String imagesImagesResult = "assets/images/images_result.svg";
+
+  /// Assets for imagesIntroIcon
+  /// assets/images/intro_icon.svg
+  static const String imagesIntroIcon = "assets/images/intro_icon.svg";
+
+  /// Assets for imagesScanIcon
+  /// assets/images/scanIcon.svg
+  static const String imagesScanIcon = "assets/images/scanIcon.svg";
+
+  /// Assets for imagesUplaodimage
+  /// assets/images/uplaodimage.svg
+  static const String imagesUplaodimage = "assets/images/uplaodimage.svg";
+}
+
